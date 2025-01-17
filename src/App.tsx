765b1b8f3779597b63304ee615e4f561f812bd4d
dcDavidCerny <dcdavidcerny@gmail.com>
2025-01-17 11:27:55 +0100

    Last Changes - Mostly language for calendarEvents

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,239 @@
+import { useState } from "react";
 import "./App.css";
 import CalendarComponent from "./Calendar";
+import styled from "@emotion/styled";
 
 function App() {
-  return <CalendarComponent />;
+  const [locale, setLocale] = useState<
+    "cs-CZ" | "en-US" | "es-ES" | "sk-SK" | "de-DE"
+  >("cs-CZ");
+  const [maxDays, setMaxDays] = useState<number | undefined>(30);
+  const [minDate, setMinDate] = useState(new Date());
+
+  const favouriteDateRangesTranslations = {
+    "cs-CZ": {
+      lastMinute: "Poslední šance",
+      janFeb: "Leden / Únor 2025",
+      summerHolidays: "Letní prázdniny 2025",
+      winterHolidays: "Zimní prázdniny 2025",
+    },
+    "en-US": {
+      lastMinute: "Last Minute",
+      janFeb: "January / February 2025",
+      summerHolidays: "Summer Holidays 2025",
+      winterHolidays: "Winter Holidays 2025",
+    },
+    "es-ES": {
+      lastMinute: "Última Hora",
+      janFeb: "Enero / Febrero 2025",
+      summerHolidays: "Vacaciones de Verano 2025",
+      winterHolidays: "Vacaciones de Invierno 2025",
+    },
+    "sk-SK": {
+      lastMinute: "Posledná šanca",
+      janFeb: "Január / Február 2025",
+      summerHolidays: "Letné prázdniny 2025",
+      winterHolidays: "Zimné prázdniny 2025",
+    },
+    "de-DE": {
+      lastMinute: "Last Minute",
+      janFeb: "Januar / Februar 2025",
+      summerHolidays: "Sommerferien 2025",
+      winterHolidays: "Winterferien 2025",
+    },
+  };
+
+  return (
+    <AppWrapper>
+      <div>
+        <div className=" bg-gray-500">
+          <h2 className="text-2xl font-bold flex justify-center p-2 mb-2 text-gray-200">
+            PROPS SETTINGS:
+          </h2>
+          <div className="propsSettings flex flex-row justify-center">
+            <div className="languageBox mr-7">
+              <h3 className="text-2xl font-bold flex justify-center">
+                language:
+              </h3>
+              <div className="languagesContainer flex flex-row justify-center">
+                <button
+                  className="p-3 m-5 w-12.5 rounded-lg bg-gray-200 hover:bg-gray-400 text-black font-bold"
+                  onClick={() => setLocale("cs-CZ")}
+                  data-selected={locale === "cs-CZ"}
+                >
+                  Czech
+                </button>
+                <button
+                  className="p-3 m-5 w-12.5 rounded-lg bg-gray-200 hover:bg-gray-400 text-black font-bold"
+                  onClick={() => setLocale("en-US")}
+                  data-selected={locale === "en-US"}
+                >
+                  English
+                </button>
+                <button
+                  className="p-3 m-5 w-12.5 rounded-lg bg-gray-200 hover:bg-gray-400 text-black font-bold"
+                  onClick={() => setLocale("es-ES")}
+                  data-selected={locale === "es-ES"}
+                >
+                  Spanish
+                </button>
+                <button
+                  className="p-3 m-5 w-12.5 rounded-lg bg-gray-200 hover:bg-gray-400 text-black font-bold"
+                  onClick={() => setLocale("sk-SK")}
+                  data-selected={locale === "sk-SK"}
+                >
+                  Slovak
+                </button>
+                <button
+                  className="p-3 m-5 w-12.5 rounded-lg bg-gray-200 hover:bg-gray-400 text-black font-bold"
+                  onClick={() => setLocale("de-DE")}
+                  data-selected={locale === "de-DE"}
+                >
+                  German
+                </button>
+              </div>
+            </div>
+
+            <div className="daysBox ml-7 mr-7">
+              <h3 className="text-2xl font-bold flex justify-center">
+                Maximum Days to Pick:
+              </h3>
+              <div className="daysContainer flex flex-row justify-center">
+                <button
+                  className="p-3 m-5 w-12.5 rounded-lg bg-gray-200 hover:bg-gray-400 text-black font-bold"
+                  onClick={() => setMaxDays(7)}
+                  data-selected={maxDays === 7}
+                >
+                  7
+                </button>
+                <button
+                  className="p-3 m-5 w-12.5 rounded-lg bg-gray-200 hover:bg-gray-400 text-black font-bold"
+                  onClick={() => setMaxDays(14)}
+                  data-selected={maxDays === 14}
+                >
+                  14
+                </button>
+                <button
+                  className="p-3 m-5 w-12.5 rounded-lg bg-gray-200 hover:bg-gray-400 text-black font-bold"
+                  onClick={() => setMaxDays(30)}
+                  data-selected={maxDays === 30}
+                >
+                  30
+                </button>
+                <button
+                  className="p-3 m-5 w-12.5 rounded-lg bg-gray-200 hover:bg-gray-400 text-black font-bold"
+                  onClick={() => setMaxDays(undefined)}
+                  data-selected={maxDays === undefined}
+                >
+                  undefined
+                </button>
+              </div>
+            </div>
+
+            <div className="minDateBox flex flex-col ml-7">
+              <h3 className="text-2xl font-bold flex justify-center">
+                Min Date to Pick:
+              </h3>
+              <input
+                className="flex justify-center text-xl mt-5"
+                type="date"
+                onChange={(e) => setMinDate(new Date(e.target.value))}
+                onClick={(e) => {
+                  const input = e.target as HTMLInputElement;
+                  input?.showPicker();
+                }}
+              />
+            </div>
+          </div>
+        </div>
+        <CalendarComponent
+          locale={locale}
+          maxDays={maxDays}
+          minDate={minDate}
+          favouriteDateRanges={[
+            {
+              name:
+                favouriteDateRangesTranslations[locale]?.lastMinute ||
+                "Poslední šance",
+              from: new Date(),
+              to: new Date(new Date().setDate(new Date().getDate() + 7)),
+            },
+            {
+              name:
+                favouriteDateRangesTranslations[locale]?.janFeb ||
+                "Leden / Únor 2025",
+              from: new Date("2025-01-01"),
+              to: new Date("2025-02-28"),
+            },
+            {
+              name:
+                favouriteDateRangesTranslations[locale]?.summerHolidays ||
+                "Letní prázdniny 2025",
+              from: new Date("2025-06-01"),
+              to: new Date("2025-08-31"),
+            },
+            {
+              name:
+                favouriteDateRangesTranslations[locale]?.winterHolidays ||
+                "Zimní prázdniny 2025",
+              from: new Date("2025-12-19"),
+              to: new Date("2026-01-02"),
+            },
+          ]}
+        />
+        ;
+      </div>
+    </AppWrapper>
+  );
 }
+
+const AppWrapper = styled.div`
+  .daysContainer button[data-selected="true"] {
+    border: 2px solid black;
+  }
+  .languagesContainer button[data-selected="true"] {
+    border: 2px solid black;
+  }
+  @media (max-width: 600px) {
+    .propsSettings {
+      flex-direction: column;
+      align-items: center;
+    }
+
+    .languageBox,
+    .daysBox,
+    .minDateBox {
+      margin: 10px 0;
+      width: 100%;
+      text-align: center;
+    }
+
+    .languagesContainer,
+    .daysContainer {
+      flex-wrap: wrap;
+    }
+
+    .languagesContainer button,
+    .daysContainer button {
+      padding: 8px;
+      margin: 5px;
+      width: auto;
+    }
+
+    input[type="date"] {
+      width: 50vw;
+      font-size: 16px;
+      margin-left: 25vw;
+    }
+
+    h2 {
+      font-size: 1.5rem;
+    }
+
+    h3 {
+      font-size: 1.25rem;
+    }
+  }
+`;
+
 export default App;
--- src/Calendar.tsx
+++ src/Calendar.tsx
@@ -2,10 +2,27 @@ import { useState } from "react";
 import ReactCalendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import "./App.css";
+import styled from "@emotion/styled";
 
-export default function CalendarComponent() {
+export interface CalendarProps {
+  locale?: string;
+  maxDays?: number;
+  minDate?: Date;
+  maxDate?: Date;
+  favouriteDateRanges?: { name: string; from: Date; to: Date }[];
+}
+export default function CalendarComponent({
+  locale,
+  minDate,
+  favouriteDateRanges,
+  maxDays,
+}: CalendarProps) {
   const [leftMonth, setLeftMonth] = useState(new Date(2025, 0, 1));
-  const [rightMonth, setRightMonth] = useState(new Date(2025, 1, 1));
+  const rightMonth = new Date(
+    leftMonth.getFullYear(),
+    leftMonth.getMonth() + 1,
+    1
+  );
 
   const [fromDate, setFromDate] = useState<Date | null>(null);
   const [toDate, setToDate] = useState<Date | null>(null);
@@ -13,83 +30,185 @@ export default function CalendarComponent() {
   const handleLeftMonthChange = ({
     activeStartDate,
   }: {
-    activeStartDate: Date;
+    activeStartDate: Date | null;
   }) => {
+    if (!activeStartDate) return;
     setLeftMonth(activeStartDate);
-    setRightMonth(
-      new Date(activeStartDate.getFullYear(), activeStartDate.getMonth() + 1, 1)
-    );
   };
 
   const handleRightMonthChange = ({
     activeStartDate,
   }: {
-    activeStartDate: Date;
+    activeStartDate: Date | null;
   }) => {
-    setRightMonth(activeStartDate);
+    if (!activeStartDate) return;
     setLeftMonth(
       new Date(activeStartDate.getFullYear(), activeStartDate.getMonth() - 1, 1)
     );
   };
 
-  const handleDateChange = (date: Date) => {
+  const handleDateChange = (date: Date | null) => {
+    if (!date) {
+      alert("Date is null");
+      return;
+    }
     if (!fromDate && !toDate) {
-      // If both fromDate and toDate are null, set first clicked date as fromDate
       setFromDate(date);
+    } else if (fromDate === date) {
+      setFromDate(null);
+      setToDate(null);
     } else if (!toDate) {
-      // If toDate is null, set it as toDate if the clicked date is later than fromDate
-      if (date > fromDate) {
+      if (date > fromDate!) {
         setToDate(date);
       } else {
-        // Swap the dates if clicked date is earlier than fromDate
         setToDate(fromDate);
         setFromDate(date);
       }
     } else {
-      // If both dates are already set, reset the range
       setFromDate(date);
       setToDate(null);
     }
   };
 
   return (
-    <div className="flex">
-      <div className="mr-5">
-        <ReactCalendar
-          onActiveStartDateChange={handleLeftMonthChange}
-          onChange={handleDateChange}
-          value={leftMonth}
-          view="month"
-        />
-      </div>
-      <div>
-        <ReactCalendar
-          onActiveStartDateChange={handleRightMonthChange}
-          onChange={handleDateChange}
-          value={rightMonth}
-          view="month"
-        />
-      </div>
-      <div>
-        <h2>Selected Dates</h2>
-        <p>
-          From: {fromDate ? fromDate.toDateString() : "None"}
-          <br />
-          To: {toDate ? toDate.toDateString() : "None"}
-        </p>
-      </div>
+    <div className="flex flex-col mt-20">
+      <FavouriteDatesWrapper className="flex justify-center">
+        {favouriteDateRanges &&
+          favouriteDateRanges.map((range) => (
+            <button
+              key={range.name}
+              className="m-2 text-sky-400 text-lg font-semibold"
+              onClick={() => {
+                setFromDate(range.from);
+                setToDate(range.to);
+              }}
+            >
+              {range.name}
+            </button>
+          ))}
+      </FavouriteDatesWrapper>
+      <BothCalendarWrapper className="flex justify-center mt-5 ">
+        <CalendarComponentWrapper>
+          <ReactCalendar
+            className="mr-5"
+            onActiveStartDateChange={handleLeftMonthChange}
+            onChange={handleDateChange}
+            activeStartDate={leftMonth}
+            view="month"
+            value={!toDate ? fromDate : [fromDate, toDate]}
+            locale={locale}
+            minDate={minDate}
+            nextLabel={null}
+            next2Label={null}
+            prev2Label={null}
+            formatDay={(_, date) => date.getDate().toString()}
+            maxDate={
+              fromDate && maxDays
+                ? new Date(fromDate.getTime() + maxDays * 24 * 60 * 60 * 1000)
+                : undefined
+            }
+          />
+        </CalendarComponentWrapper>
+        <CalendarComponentWrapper>
+          <ReactCalendar
+            onActiveStartDateChange={handleRightMonthChange}
+            onChange={handleDateChange}
+            activeStartDate={rightMonth}
+            view="month"
+            value={!toDate ? fromDate : [fromDate, toDate]}
+            locale={locale}
+            minDate={minDate}
+            prevLabel={null}
+            next2Label={null}
+            prev2Label={null}
+            formatDay={(_, date) => date.getDate().toString()}
+            maxDate={
+              fromDate && maxDays
+                ? new Date(fromDate.getTime() + maxDays * 24 * 60 * 60 * 1000)
+                : undefined
+            }
+          />
+        </CalendarComponentWrapper>
+      </BothCalendarWrapper>
     </div>
   );
 }
 
-// installed tailwind and so much more
-// so many commands to verify the installation
-// everything looks okay
-// but tailwind is not working
-// what is wrong?
-// other stuff too...
+const CalendarComponentWrapper = styled.div`
+  .react-calendar {
+    border: none;
+  }
+  .react-calendar__navigation__label__labelText,
+  .react-calendar__navigation__arrow {
+    color: rgb(0, 0, 255);
+  }
 
-// useState for from date and to date
-// if both are empty the first clicked date will be from date
-// if second clicked will be later than the first clicked date, the second clicked date will be the to date
-// if second clicked will be earlier than the first clicked date, the second date will replace the from date and the first date will be the to date
+  .react-calendar__navigation__arrow {
+    background-color: transparent !important;
+  }
+  .react-calendar__navigation__label {
+    background-color: transparent !important;
+    cursor: default !important;
+    pointer-events: none !important;
+  }
+  .react-calendar__month-view__weekdays__weekday abbr {
+    text-decoration: none;
+    color: grey;
+    font-weight: 100;
+  }
+
+  .react-calendar__month-view__days__day abbr {
+    color: black;
+    font-weight: 100;
+  }
+
+  .react-calendar__month-view__days__day--weekend abbr {
+    color: black;
+    font-weight: bold;
+  }
+
+  .react-calendar__tile--rangeStart,
+  .react-calendar__tile--rangeEnd {
+    background-color: #6fc9ec !important;
+  }
+
+  .react-calendar__tile--range {
+    background-color: #f0f0f0;
+  }
+
+  .react-calendar__month-view__days__day[disabled] {
+    background-color: transparent;
+    cursor: not-allowed;
+  }
+
+  .react-calendar__tile--now {
+    background: #bababa;
+  }
+
+  @media (max-width: 600px) {
+    .favouriteDateRangesDiv {
+      flex-direction: column;
+      flex-wrap: wrap;
+    }
+    &:first-child {
+      border-bottom: 1px solid #afafaf99;
+      padding-bottom: 10px;
+      margin-bottom: 10px;
+    }
+  }
+`;
+
+const BothCalendarWrapper = styled.div`
+  @media (max-width: 600px) {
+    flex-direction: column;
+    align-items: center;
+    margin-left: 4vw;
+  }
+`;
+
+const FavouriteDatesWrapper = styled.div`
+  @media (max-width: 600px) {
+    flex-direction: column;
+    align-items: center;
+  }
+`;
